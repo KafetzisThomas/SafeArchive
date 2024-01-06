c61375254932836a6266414483c7839f5c8cfe46
KafetzisThomas <tomkafetzis06@gmail.com>
2024-01-06 21:09:38 +0200

    Add comments to config file [CLI]

--- CLI/Scripts/cli_configs.py
+++ CLI/Scripts/cli_configs.py
@@ -5,33 +5,43 @@
 This file manages configurations and provides functionality to save and load them as well.
 """
 
-import os, json
+import os
+import json
 from pathlib import Path
 
+
 class ConfigDict(dict):
-  """Set configs"""
-  __slots__=["path"]
-  def __init__(self, config, path):
-    self.update(config)
-    self.path = Path(path)
+    """Set configs"""
+    __slots__=["path"]
 
-  def save(self):
-    """Saves config file to given path"""
-    with open(self.path, 'w') as file:
-      json.dump(self, file, indent=2)
+    def __init__(self, config: dict, path: str):
+        self.update(config)
+        self.path = Path(path)
 
-  def load(self):
-    """Loads config file from given path"""
-    with open(self.path, 'r') as file:
-      self.update(json.load(file))
+    def save(self):
+        """Saves the config file to the given path"""
+        with open(self.path, 'w') as file:
+            json.dump(self, file, indent=2)
+
+    def load(self):
+        """Loads the config file from the given path"""
+        with open(self.path, 'r') as file:
+            self.update(json.load(file))
+
 
 SETTINGS_PATH = 'settings.json'
 config = ConfigDict({
-  'source_path': None,
-  'destination_path': None,
-  'backup_to_cloud': None,
-  'backup_expiry_date': None
+    "_comments": {
+        "source_path": "List of source paths (local folders) for backups (type: list with strings)",
+        "destination_path": "Destination path (storage media) for backups (type: string)",
+        "backup_to_cloud": "Flag indicating whether to backup to the cloud (specify: storage_provider) (type: boolean)",
+        "backup_expiry_date": "Expiry date for the backups in the storage media (type: string)"
+  },
+    "source_path": ["", ],
+    "destination_path": "",
+    "backup_to_cloud": False,
+    "backup_expiry_date": None
 }, SETTINGS_PATH)
 
 if not os.path.exists(config.path):
-  config.save()
+    config.save()
