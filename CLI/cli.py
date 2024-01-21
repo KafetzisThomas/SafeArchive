99d904dfc5424985de3996bf082c17a595d82ead
KafetzisThomas <tomkafetzis06@gmail.com>
2024-01-21 22:33:47 +0200

    Fix TypeError when path is not str [CLI]

--- CLI/cli.py
+++ CLI/cli.py
@@ -33,11 +33,15 @@ colorama.init(autoreset=True)
 clear_command = "cls" if platform.system() == "Windows" else "clear"
 os.system(clear_command)  # Clear console
 
-DESTINATION_PATH = config["destination_path"] + "SafeArchive/"  # Get value from the JSON file
+try:
+    DESTINATION_PATH = config["destination_path"] + "SafeArchive/"  # Get value from the JSON file
+except TypeError:
+    pass
 
 # Run check on python version, must be 3.6 or higher because of f strings
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
-    print("Error Code U-2: This program requires running python 3.6 or higher! You are running" + str(sys.version_info[0]) + "." + str(sys.version_info[1]))
+    print("Error Code U-2: This program requires running python 3.6 or higher! You are running" +
+          str(sys.version_info[0]) + "." + str(sys.version_info[1]))
     sys.exit()
 
 try:
