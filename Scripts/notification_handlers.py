3bce763a213567f250c0392639fc0ad8609a0194
KafetzisThomas <tomkafetzis06@gmail.com>
2024-02-02 23:06:40 +0200

    Display unique icons for system notifications

--- Scripts/notification_handlers.py
+++ Scripts/notification_handlers.py
@@ -4,18 +4,23 @@
 from pathlib import Path
 from plyer import notification
 
-# Get absolute path of app icon file
+# Get absolute paths of icon files
 app_icon = str(Path("assets/ICO").joinpath("icon.ico").resolve())
+backup_completion_icon = str(Path("assets/ICO").joinpath("backup_completed.ico").resolve())
+restore_completion_icon = str(Path("assets/ICO").joinpath("restore.ico").resolve())
+drive_icon = str(Path("assets/ICO").joinpath("drive.ico").resolve())
+cloud_icon = str(Path("assets/ICO").joinpath("cloud.ico").resolve())
+file_missing_icon = str(Path("assets/ICO").joinpath("file_missing.ico").resolve())
 
 
 def notify_backup_completion(DESTINATION_PATH, notifications):
     """Display notification message when backup process successfully completes"""
     if notifications:
         notification.notify(
-            title="Backup Completed",
+            title="SafeArchive: Backup Completed",
             app_name="SafeArchive",
             message=f"SafeArchive has finished the backup to '{DESTINATION_PATH.replace('SafeArchive/', '')}'.",
-            app_icon=app_icon,
+            app_icon=backup_completion_icon,
             timeout=10
         )
 
@@ -24,10 +29,10 @@ def notify_restore_completion(notifications):
     """Display notification message when restore process successfully completes"""
     if notifications:
         notification.notify(
-            title="Files Restored Sucessfully",
+            title="SafeArchive: Files Restored Sucessfully",
             app_name="SafeArchive",
             message=f"SafeArchive has finished the restore.",
-            app_icon=app_icon,
+            app_icon=restore_completion_icon,
             timeout=10
         )
 
@@ -36,10 +41,10 @@ def notify_drive_reconnection(notifications):
     """Display notification message when drive was disconnected / for too long"""
     if notifications:
         notification.notify(
-            title="Reconnect your drive",
+            title="SafeArchive: Reconnect your drive",
             app_name="SafeArchive",
             message="Your SafeArchive Drive was disconnected for too long. Reconnect it to keep saving copies of your files.",
-            app_icon=app_icon,
+            app_icon=drive_icon,
             timeout=10
         )
 
@@ -48,10 +53,10 @@ def notify_drive_space_limitation(notifications):
     """Display notification message when drive storage is running out"""
     if notifications:
         notification.notify(
-            title="Warning: Your Drive storage is running out.",
+            title="SafeArchive: [Warning] Your Drive storage is running out.",
             app_name="SafeArchive",
             message="Your Drive storage is almost full. To make sure your files can sync, clean up space.",
-            app_icon=app_icon,
+            app_icon=drive_icon,
             timeout=10
         )
 
@@ -60,19 +65,19 @@ def notify_cloud_space_limitation(notifications):
     """Display notification message when cloud storage is running out"""
     if notifications:
         notification.notify(
-            title="Warning: Your Google Drive storage is running out.",
+            title="SafeArchive: [Warning] Your Google Drive storage is running out.",
             app_name="SafeArchive",
             message="Your Google Drive storage is almost full. To make sure your files can sync, clean up space.",
-            app_icon=app_icon,
+            app_icon=cloud_icon,
             timeout=10
         )
 
 def notify_client_secrets_file_missing(notifications):
     if notifications:
         notification.notify(
-            title="Error: File 'client_secrets.json' is missing.",
+            title="SafeArchive: [Error] File 'client_secrets.json' is missing.",
             app_name="SafeArchive",
             message="File not found in the program directory. Please refer to the documentation for instructions on how to get it.",
-            app_icon=app_icon,
+            app_icon=file_missing_icon,
             timeout=10
         )
