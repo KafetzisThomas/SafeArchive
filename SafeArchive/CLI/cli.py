31e0da6e403a849565315486f35ee4d0539b1308
KafetzisThomas <tomkafetzis06@gmail.com>
2024-08-09 10:01:45 +0300

    Add function to display configuration info [CLI]

--- SafeArchive/CLI/cli.py
+++ SafeArchive/CLI/cli.py
@@ -13,15 +13,21 @@ import time
 import platform
 
 # Append the parent directory to the sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 # Import module files
 from SafeArchive.Scripts.CLI.backup_utils import Backup
 from SafeArchive.Scripts.CLI.restore import RestoreBackup
-from SafeArchive.Scripts.file_utils import get_backup_size, storage_media_free_space, last_backup, create_destination_directory_path
-from SafeArchive.Scripts.CLI.file_utils import edit_configs
+from SafeArchive.Scripts.file_utils import (
+    get_backup_size,
+    storage_media_free_space,
+    last_backup,
+    create_destination_directory_path,
+)
+from SafeArchive.Scripts.CLI.file_utils import get_config_info, edit_configs
 from SafeArchive.Scripts.system_notifications import notify_user
 from SafeArchive.Scripts.configs import config
+
 config.load()
 
 # Import other (third-party) modules
@@ -29,6 +35,7 @@ import humanize
 import colorama
 from art import text2art
 from colorama import Fore as F, Back as B
+
 colorama.init(autoreset=True)
 
 backup = Backup()
@@ -39,14 +46,20 @@ clear_command = "cls" if platform.system() == "Windows" else "clear"
 os.system(clear_command)
 
 try:
-    DESTINATION_PATH = config["destination_path"] + "SafeArchive/"  # Get value from the JSON file
+    DESTINATION_PATH = (
+        config["destination_path"] + "SafeArchive/"
+    )  # Get value from the JSON file
 except TypeError:
     pass
 
 # Run check on python version, must be 3.6 or higher because of f strings
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
-    print("Error Code U-2: This program requires running python 3.6 or higher! You are running" +
-          str(sys.version_info[0]) + "." + str(sys.version_info[1]))
+    print(
+        "Error Code U-2: This program requires running python 3.6 or higher! You are running"
+        + str(sys.version_info[0])
+        + "."
+        + str(sys.version_info[1])
+    )
     sys.exit()
 
 try:
@@ -57,7 +70,9 @@ except IndexError:
     pass
 
 if config["source_paths"] is None or config["destination_path"] is None:
-    print(f"{B.RED}{F.WHITE} NOTE {B.RESET}{F.RESET} Please specify your preferences in {F.LIGHTYELLOW_EX}settings.json{F.RESET} file...")
+    print(
+        f"{B.RED}{F.WHITE} NOTE {B.RESET}{F.RESET} Please specify your preferences in {F.LIGHTYELLOW_EX}settings.json{F.RESET} file..."
+    )
     print("\nREQUIRED:")
     print("'source_paths': [path/to/, path/to/, ...] --> string inside list")
     print("'destination_path': 'path' --> string")
@@ -78,13 +93,25 @@ create_destination_directory_path(DESTINATION_PATH)
 print(text2art("SafeArchive-CLI"))
 print(f"> Author: {F.LIGHTYELLOW_EX}KafetzisThomas")
 print("-------------------------")
-print(f"\n~ Last Backup: {B.LIGHTBLUE_EX}{F.WHITE} {last_backup(DESTINATION_PATH)} {B.RESET}{F.RESET}")
-print(f"~ Free space on ({DESTINATION_PATH.replace('SafeArchive/', '')}): {storage_media_free_space()} GB")
+print(
+    f"\n~ Last Backup: {B.LIGHTBLUE_EX}{F.WHITE} {last_backup(DESTINATION_PATH)} {B.RESET}{F.RESET}"
+)
+print(
+    f"~ Free space on ({DESTINATION_PATH.replace('SafeArchive/', '')}): {storage_media_free_space()} GB"
+)
 print(f"~ Size of backup: {humanize.naturalsize(get_backup_size(DESTINATION_PATH))}")
-print(f"\n1. {F.LIGHTMAGENTA_EX}Backup{F.RESET} Now"
-      f" - Zip source path files to {F.LIGHTCYAN_EX}destination{F.RESET} path")
-print(f"2. Restore {F.LIGHTGREEN_EX}previous{F.RESET} backup"
-      f" - {F.LIGHTBLACK_EX}Extract{F.RESET} selected zip file")
+print(
+    f"\n1. {F.LIGHTMAGENTA_EX}Config{F.RESET} Info"
+    f" - Display {F.LIGHTCYAN_EX}your{F.RESET} preferences"
+)
+print(
+    f"2. {F.LIGHTMAGENTA_EX}Backup{F.RESET} Now"
+    f" - Zip source path files to {F.LIGHTCYAN_EX}destination{F.RESET} path"
+)
+print(
+    f"3. Restore {F.LIGHTGREEN_EX}previous{F.RESET} backup"
+    f" - {F.LIGHTBLACK_EX}Extract{F.RESET} selected zip file"
+)
 
 try:
     choice = int(input("\nChoice (1-2): "))
@@ -96,16 +123,20 @@ except KeyboardInterrupt:
     sys.exit()
 
 if choice == 1:
+    get_config_info()
+elif choice == 2:
     try:
         start = time.time()
-        backup.perform_backup(SOURCE_PATHS=config['source_paths'], DESTINATION_PATH=DESTINATION_PATH)
+        backup.perform_backup(
+            SOURCE_PATHS=config["source_paths"], DESTINATION_PATH=DESTINATION_PATH
+        )
         end = time.time()
         time_elapsed = end - start
         print(f"[Finished in {time_elapsed:.1f}s]")
     except KeyboardInterrupt:
         notify_user(message="Backup process cancelled.", terminal_color=F.LIGHTRED_EX)
         sys.exit()
-elif choice == 2:
+elif choice == 3:
     restore_backup.run_restore_thread(DESTINATION_PATH=DESTINATION_PATH)
 else:
     notify_user(message="Undefined choice.", terminal_color=F.LIGHTRED_EX)
--- SafeArchive/Scripts/CLI/file_utils.py
+++ SafeArchive/Scripts/CLI/file_utils.py
@@ -6,8 +6,40 @@ import colorama
 import platform
 from SafeArchive.Scripts.configs import ConfigDict
 from SafeArchive.Scripts.system_notifications import notify_user
+from SafeArchive.Scripts.configs import config
 from colorama import Fore as F, Back as B
+
 colorama.init(autoreset=True)
+
+
+def get_config_info():
+    print("Config Info:")
+    print(f"\n{F.LIGHTGREEN_EX}Platform:{F.RESET} {config['platform']}")
+    print(f"{F.LIGHTGREEN_EX}Source paths:{F.RESET} {config['source_paths']}")
+    print(f"{F.LIGHTGREEN_EX}Destination path:{F.RESET} {config['destination_path']}")
+    print(f"{F.LIGHTGREEN_EX}Notifications:{F.RESET} {config['notifications']}")
+    print(f"{F.LIGHTGREEN_EX}System tray:{F.RESET} {config['system_tray']}")
+    print(f"{F.LIGHTGREEN_EX}Encryption:{F.RESET} {config['encryption']}")
+    print(f"{F.LIGHTGREEN_EX}Appearance mode:{F.RESET} {config['appearance_mode']}")
+    print(f"{F.LIGHTGREEN_EX}Color theme:{F.RESET} {config['color_theme']}")
+    print(
+        f"{F.LIGHTGREEN_EX}Backup expiry date:{F.RESET} {config['backup_expiry_date']}"
+    )
+    print(f"{F.LIGHTGREEN_EX}Storage provider:{F.RESET} {config['storage_provider']}")
+    print(
+        f"{F.LIGHTGREEN_EX}Compression method:{F.RESET} {config['compression_method']}"
+    )
+    print(f"{F.LIGHTGREEN_EX}Allow Zip64:{F.RESET} {config['allowZip64']}")
+    print(f"{F.LIGHTGREEN_EX}Compression level:{F.RESET} {config['compression_level']}")
+    print(f"{F.LIGHTGREEN_EX}Backup interval:{F.RESET} {config['backup_interval']}")
+    print(f"{F.LIGHTGREEN_EX}FTP hostname:{F.RESET} {config['ftp_hostname']}")
+    print(f"{F.LIGHTGREEN_EX}FTP username:{F.RESET} {config['ftp_username']}")
+    print(f"{F.LIGHTGREEN_EX}FTP password:{F.RESET} {config['ftp_password']}")
+    print(f"{F.LIGHTGREEN_EX}Mega email:{F.RESET} {config['mega_email']}")
+    print(f"{F.LIGHTGREEN_EX}Mega password:{F.RESET} {config['mega_password']}")
+    print(
+        f"{F.LIGHTGREEN_EX}Dropbox access token:{F.RESET} {config['dropbox_access_token']}\n"
+    )
 
 
 def edit_configs():
@@ -15,26 +47,52 @@ def edit_configs():
 
     try:
         print(f"{B.GREEN} -- CONFIGURATION -- {B.RESET}")
-        folder_to_backup = input(f"{B.RED}! TO SET MORE THAN ONE SOURCE PATH, EDIT THE CONFIG FILE !\n! YOU NEED TO INPUT THE WHOLE PATH !{B.RESET}\nPlease enter the folder to backup: ")
+        folder_to_backup = input(
+            f"{B.RED}! TO SET MORE THAN ONE SOURCE PATH, EDIT THE CONFIG FILE !\n! YOU NEED TO INPUT THE WHOLE PATH !{B.RESET}\nPlease enter the folder to backup: "
+        )
         backup_destination = input("Please enter the destination folder: ")
-        backup_expire = input("if you want to set the expiry date of backups, enter here(leave None if you don't want): ")
+        backup_expire = input(
+            "if you want to set the expiry date of backups, enter here(leave None if you don't want): "
+        )
         encryption = input("do you want to encrypt your backups?(y/N): ")
-        compression_method = input("Please enter the compression method for your backups\n(ZIP_DEFLATED, ZIP_STORED, ZIP_LZMA, ZIP_BZIP2): ")
-        allowZip64 = input("Do you want to allow Zip64 (backups larger than 4gb)?(y/N): ")
-        compression_level = input("Enter compression level for your backups (1: fast, ... 9: saves storage space): ")
-        storage_provider = input("if you want to use a storage provider along with your drive, enter here (Google Drive, FTP, MegaCloud, Dropbox)\n(leave None if you don't want): ")
-        ftp_hostname = input("if you have set FTP as your storage provider, enter hostname here: ")
-        ftp_username = input("if you have set FTP as your storage provider, enter username here: ")
-        ftp_password = input("if you have set FTP as your storage provider, enter password here: ")
-        mega_email = input("if you have set Mega as your storage provider, enter email here: ")
-        mega_password = input("if you have set Mega as your storage provider, enter password here: ")
-        dropbox_access_token = input("if you have set Dropbox as your storage provider, enter token here: ")
+        compression_method = input(
+            "Please enter the compression method for your backups\n(ZIP_DEFLATED, ZIP_STORED, ZIP_LZMA, ZIP_BZIP2): "
+        )
+        allowZip64 = input(
+            "Do you want to allow Zip64 (backups larger than 4gb)?(y/N): "
+        )
+        compression_level = input(
+            "Enter compression level for your backups (1: fast, ... 9: saves storage space): "
+        )
+        storage_provider = input(
+            "if you want to use a storage provider along with your drive, enter here (Google Drive, FTP, MegaCloud, Dropbox)\n(leave None if you don't want): "
+        )
+        ftp_hostname = input(
+            "if you have set FTP as your storage provider, enter hostname here: "
+        )
+        ftp_username = input(
+            "if you have set FTP as your storage provider, enter username here: "
+        )
+        ftp_password = input(
+            "if you have set FTP as your storage provider, enter password here: "
+        )
+        mega_email = input(
+            "if you have set Mega as your storage provider, enter email here: "
+        )
+        mega_password = input(
+            "if you have set Mega as your storage provider, enter password here: "
+        )
+        dropbox_access_token = input(
+            "if you have set Dropbox as your storage provider, enter token here: "
+        )
 
         # Rewrite values for *efficiency*
         encryption = True if encryption.lower() == "y" else False
         backup_expire = backup_expire if backup_expire else None
         storage_provider = storage_provider if storage_provider else "None"
-        compression_method = compression_method if compression_method else "ZIP_DEFLATED"
+        compression_method = (
+            compression_method if compression_method else "ZIP_DEFLATED"
+        )
         allowZip64 = allowZip64 if allowZip64 else "True"
         compression_level = compression_level if compression_level else 5
         backup_interval = None
@@ -45,26 +103,28 @@ def edit_configs():
         mega_password = mega_password if mega_password else ""
         dropbox_access_token = dropbox_access_token if dropbox_access_token else ""
 
-
-        SETTINGS_PATH = 'settings.json'
-        config = ConfigDict({
-            "platform": platform.system(),
-            "source_paths": [folder_to_backup],
-            "destination_path": backup_destination,
-            "encryption": encryption,
-            "backup_expiry_date": backup_expire,
-            "compression_method": compression_method,
-            "allowZip64": allowZip64,
-            "compression_level": compression_level,
-            "backup_interval": backup_interval,
-            "storage_provider": storage_provider,
-            "ftp_hostname": ftp_hostname,
-            "ftp_username": ftp_username,
-            "ftp_password": ftp_password,
-            "mega_email": mega_email,
-            "mega_password": mega_password,
-            "dropbox_access_token": dropbox_access_token
-        }, SETTINGS_PATH)
+        SETTINGS_PATH = "settings.json"
+        config = ConfigDict(
+            {
+                "platform": platform.system(),
+                "source_paths": [folder_to_backup],
+                "destination_path": backup_destination,
+                "encryption": encryption,
+                "backup_expiry_date": backup_expire,
+                "compression_method": compression_method,
+                "allowZip64": allowZip64,
+                "compression_level": compression_level,
+                "backup_interval": backup_interval,
+                "storage_provider": storage_provider,
+                "ftp_hostname": ftp_hostname,
+                "ftp_username": ftp_username,
+                "ftp_password": ftp_password,
+                "mega_email": mega_email,
+                "mega_password": mega_password,
+                "dropbox_access_token": dropbox_access_token,
+            },
+            SETTINGS_PATH,
+        )
 
         config_details = f"""
 This is the configuration:
