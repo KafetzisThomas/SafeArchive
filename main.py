a5fdef7cc652b509c9ce169c58a364ef3dc3e260
KafetzisThomas <tomkafetzis06@gmail.com>
2025-12-16 17:03:39 +0200

    Simple reorder of startup variables

--- main.py
+++ main.py
@@ -26,10 +26,9 @@ from PyQt6.QtWidgets import (
 )
 
 version = "1.5.0"
+
 config.load()  # load the json file into memory
-
-DESTINATION_PATH = config["destination_path"] + "SafeArchive/"  # get value from the json file
-
+DESTINATION_PATH = config["destination_path"] + "SafeArchive/"
 
 if len(sys.argv) > 1 and sys.argv[1] == "--nogui":
     runpy.run_path("cli.py")
